53e9f0c7bb280e4c201d7587e62c0ac7124a38ff
NYKOL4Z <nico.oddoneout@gmail.com>
2016-01-24 19:27:04 -0300

    Test version

--- app/src/main/java/games/primitive/musicalchairs/MainActivity.java
+++ app/src/main/java/games/primitive/musicalchairs/MainActivity.java
@@ -144,6 +144,7 @@ public class MainActivity extends AppCompatActivity {
 
 	public Handler pauseHandler = new Handler();
 	public void playMusic(View view) {
+		Toast.makeText(this, "V2", Toast.LENGTH_SHORT);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		if (customMusic != null) {
 			music = customMusic;
